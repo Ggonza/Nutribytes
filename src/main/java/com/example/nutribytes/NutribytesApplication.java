36bfd7bf5a41db7ee37a1e6f46b63d5d36ca7b30
alexander gonzalez <alexander.gonzalez@gtd.com.co>
2023-04-22 11:18:01 -0500

    Initial commit

---
+++ src/main/java/com/example/nutribytes/NutribytesApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutribytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutribytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutribytesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nutribytes/NutribytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutribytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutribytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
